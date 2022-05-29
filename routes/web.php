9f1971089ede5bbcfa04e244e66650667b81faa2
daraholic <1112dara@gmail.com>
2022-05-29 13:48:37 +0800

    show list ok

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ListController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/messageboard', function () {
-    return 1;
-});
+// Route::get('/messageboard', function () {
+//     return 1;
+// });
+Route::get('/messageboard', [ListController::class, 'list']);
